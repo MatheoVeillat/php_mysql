6327ac5751dbd97822e84793fb54041e9a6a98f3
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-09-06 11:56:44 +0200

    Les tableaux associatifs

--- AfficherRecette.php
+++ AfficherRecette.php
@@ -1,25 +1,32 @@
 <?php
-
- // Déclaration du tableau des recettes
- $recipes = [
-    ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-    ['Couscous','[...]','mickael.andrieu@exemple.com',false,],
- ];
-
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'prenez une belle',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ]
+];
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Affichage des recettes</title>
-</head>
-<body>
-    <ul>
-        <?php for ($lines = 0; $lines <= 1; $lines++): ?>
-        <li>
-            <?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-        </li>
-        <?php endfor; ?>
-    </ul>
-</body>
+    <head>
+        <title>Affichage des recettes</title>
+    </head>
+    <body>
+    <?php
+        foreach($recipes as $recipe) {?>
+            <h2>  <?php echo $recipe['title'] ?></h2>
+            <p> Etape 1 : <?php echo $recipe['recipe'] ?></p>
+            <p> <?php echo $recipe['author'] ?></p>
+            <?php }?>
+        
+    </body>
 </html>
