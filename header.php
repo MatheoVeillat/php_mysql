1ec1d820a19f75fd1b935d4e71d2f857a9b958b2
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-09-12 15:35:55 +0200

    Découpage d'une page web

---
+++ header.php
@@ -0,0 +1,10 @@
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Menu de navigation</h3>
+        <ul>
+            <li><a href="index.php">Lien</a></li>
+            <li><a href="index.php">Lien</a></li>
+            <li><a href="index.php">Lien</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon site de recettes</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+        <!-- Le menu -->
+    
+        <?php include('header.php'); ?>
+       
+    </header>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Mon site de recettes</h1>
+                
+            <p>
+                Bienvenue sur mon site de recettes !
+            </p>
+        </div>
+    
+    <!-- Le pied de page -->
+    
+        <?php include('footer.php'); ?>
+    
+    </body>
+</html>
