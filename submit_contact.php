4745c9e8ff257a429a17ce46ff351196674b18bf
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-09-12 15:55:02 +0200

    Récupérez les paramètres en PHP

--- submit_contact.php
+++ submit_contact.php
@@ -58,7 +58,8 @@
         }
         ?>
         
-        <h1>Message bien reçu !</h1>
+        <h1>Message bien reçus !</h1>
+
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Rappel de vos informations</h5>
