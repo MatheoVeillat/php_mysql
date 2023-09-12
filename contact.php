026a6b26e3bbb29502ea411f431fe974c3443c16
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-09-12 15:39:00 +0200

    Projet fil rouge

---
+++ contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+	<h3>Site de recettes</h3>
+	<div>
+		<h2>Contactez nous</h2>
+		<br>
+			<label for="email"> Email:</label>
+			<br>
+			<input type="email" id="email" name="email" placeholder="yourmail@domain.com">
+			<br>
+			<p>Nous ne revendrons pas votre email.</p>
+		</div>
+		<div>
+			<br>
+			<p>Votre message </p>
+			<textarea placeholder="Remarque"></textarea>
+		</div>
+		<div>
+			<br>
+			<input type="submit" value="Envoyer">
+		</div>
+		<footer>
+			<p> 2021 Copyright: Alexandre Brabant </p>
+		</footer>
+	</body>
+</html>
