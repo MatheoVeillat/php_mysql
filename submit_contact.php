f0ea389c0476c121ebf6133c0924d581bb7d3a9a
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-09-13 10:27:38 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- submit_contact.php
+++ submit_contact.php
@@ -58,8 +58,7 @@
         }
         ?>
         
-        <h1>Message bien reçus !</h1>
-
+        <h1>Message bien reçu !</h1>
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Rappel de vos informations</h5>
