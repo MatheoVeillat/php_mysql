604277835e99e6473e8fbf78a6eef233e20399c2
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-10-04 11:03:16 +0200

    Effectuez une première requête SQL

--- mysql.php
+++ mysql.php
@@ -10,10 +10,26 @@ try {
         sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8',
         MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
         MYSQL_USER,
-        MYSQL_PASSWORD
+        MYSQL_PASSWORD,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $exception) {
     die('Erreur : '.$exception->getMessage());
 }
 ?>
+
+<?php
+// On récupère tout le contenu de la table recipes
+$sqlQuery = 'SELECT * FROM recipes WHERE is_enabled = 1';
+$recipesStatement = $db->prepare($sqlQuery);
+$recipesStatement->execute();
+$recipes = $recipesStatement->fetchAll();
+?>
+<!-- On affiche chaque recette une à une -->
+<?php foreach ($recipes as $recipe) : ?>
+    <p><?php echo $recipe['title']; ?></p>
+    <p>Auteur : <?php echo $recipe['author']; ?></p>
+    <p>Recette : <?php echo $recipe['recipe']; ?></p>
+    <p>---------------------</p>
+<?php endforeach; ?>
+
