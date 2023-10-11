e26b7b3d0b08ea67b429a1bf8c3e2ffb53ded094
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-10-11 16:45:34 +0200

    Isolez l'affichage du traitement PHP

--- blog/index.php
+++ blog/index.php
@@ -1,24 +1,8 @@
 <?php
-try {
-    $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Add this line for error handling
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
 
-// Retrieve the 5 latest posts
-$statement = $database->query(
-   "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
-);
-$posts = [];
-while ($row = $statement->fetch()) {
-    $post = [
-        'title' => $row['titre'], // Fixed typo in variable name
-        'content' => $row['contenu'],
-        'frenchCreationDate' => $row['date_creation_fr'],
-    ];
-    $posts[] = $post; // Add this line to append the post to the $posts array
-}
+require('src/model.php');
+
+$posts = getPosts();
 
 require('templates/homepage.php');
 ?>
---
+++ blog/src/model.php
@@ -0,0 +1,26 @@
+<?php
+function getPosts() {
+    // We connect to the database.
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Add this line for error handling
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+    
+    // Retrieve the 5 latest posts
+    $statement = $database->query(
+       "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+    );
+    $posts = [];
+    while ($row = $statement->fetch()) {
+        $post = [
+            'title' => $row['titre'], // Fixed typo in variable name
+            'content' => $row['contenu'],
+            'frenchCreationDate' => $row['date_creation_fr'],
+        ];
+        $posts[] = $post; // Add this line to append the post to the $posts array
+    }
+    return $posts;
+}
+?>
