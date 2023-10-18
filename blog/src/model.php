ed77cace6b017545c2572edbeb681c077faa83b8
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-10-18 13:52:08 +0200

    Base pour créer un template

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,26 +1,71 @@
 <?php
+
 function getPosts() {
-    // We connect to the database.
-    try {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Add this line for error handling
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-    
-    // Retrieve the 5 latest posts
+    $database = dbConnect();
     $statement = $database->query(
-       "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
     $posts = [];
-    while ($row = $statement->fetch()) {
+    while (($row = $statement->fetch())) {
         $post = [
-            'title' => $row['titre'], // Fixed typo in variable name
-            'content' => $row['contenu'],
-            'frenchCreationDate' => $row['date_creation_fr'],
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
+            'identifier' => $row['id'],
         ];
-        $posts[] = $post; // Add this line to append the post to the $posts array
+
+        $posts[] = $post;
     }
+
     return $posts;
 }
 
+function getPost($identifier) {
+    $database = dbConnect();
+    $statement = $database->prepare(
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
+    );
+    $statement->execute([$identifier]);
+
+    $row = $statement->fetch();
+    $post = [
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+    ];
+
+    return $post;
+}
+
+function getComments($identifier)
+{
+    $database = dbConnect();
+    $statement = $database->prepare(
+        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
+    );
+    $statement->execute([$identifier]);
+
+    $comments = [];
+    while (($row = $statement->fetch())) {
+        $comment = [
+            'author' => $row['author'],
+            'french_creation_date' => $row['french_creation_date'],
+            'comment' => $row['comment'],
+        ];
+
+        $comments[] = $comment;
+    }
+
+    return $comments;
+}
+
+function dbConnect()
+{
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+
+        return $database;
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+}
--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -1,33 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title>Le blog de l'AVBN</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <title>Le blog de l'AVBN</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
 
-<body>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p>Derniers billets du blog :</p>
-    <?php
-    foreach ($posts as $post) {
-    ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($post['title']); ?>
-                <em>le <?php echo $post['frenchCreationDate']; ?></em>
-            </h3>
-            <p>
-                <?php
-                // Display the content of the post
-                echo nl2br(htmlspecialchars($post['content']));
-                ?>
-                <br />
-                <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-    <?php
-    } // End of the post loop
-    ?>
-</body>
+    <body>
+        <h1>Le super blog de l'AVBN !</h1>
+        <p>Derniers billets du blog :</p>
+
+        <?php
+        foreach ($posts as $post) {
+        ?>
+            <div class="news">
+                <h3>
+                    <?= htmlspecialchars($post['title']); ?>
+                    <em>le <?= $post['french_creation_date']; ?></em>
+                </h3>
+                <p>
+                    <?= nl2br(htmlspecialchars($post['content'])); ?>
+                    <br />
+                    <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+                </p>
+            </div>
+        <?php
+        }
+        ?>
+    </body>
 </html>
