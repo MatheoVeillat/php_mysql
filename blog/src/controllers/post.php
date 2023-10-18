79c3673ba80dddc9715761b0891a8c87926b8f3a
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-10-18 15:25:14 +0200

    Ajoutez des commentaires

--- blog/src/controllers/post.php
+++ blog/src/controllers/post.php
@@ -1,7 +1,8 @@
 <?php
 // src/controllers/post.php
 require_once('src/model.php');
-function post( $identifier)
+require_once('src/model/comment.php');
+function post($identifier)
 {
     $post = getPost($identifier);
     $comments = getComments($identifier);
