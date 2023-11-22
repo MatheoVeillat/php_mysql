6882360c9dde5416659a3e9b3caab87a3b9a0fec
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-11-22 10:57:18 +0100

    Contraignez l'usage de vos classes

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 
-/*
- * This file is part of the OpenClassRoom PHP Object Course.
- *
- * (c) Grégoire Hébert <contact@gheb.dev>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+abstract class Player
+{
+    protected $name;
+    protected $ratio;
 
-class QueuingPlayer extends Player 
+    public function __construct($name, $ratio = 400.0)
+    {
+        $this->name = $name;
+        $this->ratio = $ratio;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getRatio()
+    {
+        return $this->ratio;
+    }
+
+    protected final function probabilityAgainst(Player $player)
+    {
+        return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
+    }
+
+    public final function updateRatioAgainst(Player $player, $result): void
+    {
+        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
+    }
+}
+
+final class QueuingPlayer extends Player
 {
     private $range;
 
@@ -19,69 +42,47 @@ class QueuingPlayer extends Player
         $this->range = $range;
     }
 
-    public function getRange()
+    public function getRange(): int
     {
         return $this->range;
     }
 }
 
-
 class Lobby
 {
     public $queuingPlayers = [];
 
-    public function findOponents(QueuingPlayer $player) {
+    public function findOpponents(QueuingPlayer $player)
+    {
         $minLevel = round($player->getRatio() / 100);
         $maxLevel = $minLevel + $player->getRange();
 
-        return array_filter($this->queuingPlayers, function ($potentialOponent) use ($minLevel, $maxLevel, $player) {
-            $playerLevel = round($potentialOponent->getRatio() / 100);
+        return array_filter($this->queuingPlayers, function ($potentialOpponent) use ($minLevel, $maxLevel, $player) {
+            $playerLevel = round($potentialOpponent->getRatio() / 100);
 
-            return $player !== $potentialOponent && ($minLevel <= $playerLevel) && ($playerLevel <= $maxLevel);
+            return $player !== $potentialOpponent && ($minLevel <= $playerLevel) && ($playerLevel <= $maxLevel);
         });
     }
 
-    public function addPlayer(Player $player) {
-        $this->queuingPlayers[] = new QueuingPlayer($player);
+    public function addPlayer(Player $player)
+    {
+        $this->queuingPlayers[] = new QueuingPlayer($player->getName(), $player->getRatio());
     }
 
-    public function addPlayers(Player ...$players) {
+    public function addPlayers(Player ...$players)
+    {
         foreach ($players as $player) {
             $this->addPlayer($player);
         }
     }
 }
+$greg = new QueuingPlayer('greg', 400);
+$jade = new QueuingPlayer('jade', 476);
 
-class Player
-{
-    public function __construct($name, $ratio = 400.0) {
-        $this->name = $name;
-        $this->ratio = $ratio;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    private function probabilityAgainst($player) {
-        return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
-    }
-
-    public function updateRatioAgainst($player, $result) {
-        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
-    }
-
-    public function getRatio() {
-        return $this->ratio;
-    }
-}
-
-$greg = new Player('greg', 400);
-$jade = new Player('jade', 476);
 
 $lobby = new Lobby();
 $lobby->addPlayers($greg, $jade);
 
-var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
+var_dump($lobby->findOpponents($lobby->queuingPlayers[0]));
 
 exit(0);
