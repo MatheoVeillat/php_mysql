b51b7268b107176549e8a220a6fb3323e2a83c68
Matheo Veillat <95935150+MatheoVeillat@users.noreply.github.com>
2023-12-13 11:12:45 +0100

    Structurez vos fichiers 2.0

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the OpenClassRoom PHP Object Course.
- *
- * (c) Grégoire Hébert <contact@gheb.dev>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 spl_autoload_register(static function ($fqcn): void {
@@ -16,9 +7,20 @@ spl_autoload_register(static function ($fqcn): void {
     require_once $path;
 });
 
-use App\Domain\MatchMaker\Encounter\Score;
-use App\Domain\MatchMaker\Lobby;
-use App\Domain\MatchMaker\Player\Player;
+namespace {
+    use App\MatchMaker\Lobby;
+    use App\MatchMaker\Player\Player;
+
+    $greg = new Player('greg');
+    $jade = new Player('jade');
+
+    $lobby = new Lobby();
+    $lobby->addPlayers($greg, $jade);
+
+    var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
+
+    exit(0);
+}
 
 $greg = new Player('greg');
 $chuckNorris = new Player('Chuck Norris', 3000);
